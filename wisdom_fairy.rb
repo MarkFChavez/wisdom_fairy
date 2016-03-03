66836d1a07087cbea7a726e307a1710598323d06
Mark Chavez <markchav3z@gmail.com>
2016-03-03 20:35:52 +0800

    constant

--- wisdom_fairy.rb
+++ wisdom_fairy.rb
@@ -1,10 +1,12 @@
-quotes = [
-  { text: "\"Whenever you find yourself on the side of the majority, it is time to reform (or pause and reflect).\"", author: "Mark Twain" },
-  { text: "\"Anyone who thinks sitting in church can make you a Christian must also think that sitting in a garage can make you a car.\"", author: "Garrison Keillor" },
-  { text: "\"It is impossible to live without failing at something, unless you live so cautiously that you might as well not have lived at all - in which case, you fail by default.\"", author: "J.K. Rowling" }
-]
-
 class WisdomFairy
+  class << self
+    QUOTES = [
+      { text: "\"Whenever you find yourself on the side of the majority, it is time to reform (or pause and reflect).\"", author: "Mark Twain" },
+      { text: "\"Anyone who thinks sitting in church can make you a Christian must also think that sitting in a garage can make you a car.\"", author: "Garrison Keillor" },
+      { text: "\"It is impossible to live without failing at something, unless you live so cautiously that you might as well not have lived at all - in which case, you fail by default.\"", author: "J.K. Rowling" }
+    ]
+  end
+
   def self.get!
     uri = URI.parse("http://quotes.rest/qod.json?category=life")
     http = Net::HTTP.new(uri.host, uri.port)
@@ -12,7 +14,7 @@ class WisdomFairy
     begin
       Quote.new(JSON.parse(response.body)["contents"]["quotes"].first)
     rescue
-      quote = quotes.sample
+      quote = QUOTES.sample
       Quote.new({ quote: quote.text, author: quote.author})
     end
 
