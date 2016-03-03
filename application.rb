f5b69ba234aa681b98e81ba30addbe424d421bfb
Mark Chavez <markchav3z@gmail.com>
2016-03-03 20:17:32 +0800

    fix

--- application.rb
+++ application.rb
@@ -9,13 +9,21 @@ require_relative "wisdom_fairy"
 
 puts "Getting Quote of the Day..."
 
+quotes = [
+  "\"Whenever you find yourself on the side of the majority, it is time to reform (or pause and reflect).\" - Mark Twain",
+  "\"Anyone who thinks sitting in church can make you a Christian must also think that sitting in a garage can make you a car.\" - Garrison Keillor",
+  "\"It is impossible to live without failing at something, unless you live so cautiously that you might as well not have lived at all - in which case, you fail by default.\" - J.K. Rowling"
+]
+
+quote = "#{WisdomFairy.get!}" rescue quotes.sample
+
 @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
 
 begin
   @client.messages.create(
     from: TWILIO_NUMBER,
     to: RECIPIENTS,
-    body: QUOTE_OF_THE_DAY
+    body: quote
   )
 rescue Twilio::REST::RequestError => e
   puts e.message
